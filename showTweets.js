427f9a1e13913cb12687e0fba024e5d34e2e353a
Semen Makhaev <semenmakhaev@yandex.ru>
2017-04-30 22:48:52 +0500

    Реализована функция showTweets

--- showTweets.js
+++ showTweets.js
@@ -1,8 +1,33 @@
+'use strict';
+
+const request = require('request');
 const formatDate = require('./formatDate');
 
-function showTweets() {
-    // Здесь будет код, который получает твиты и
-    // выводит их на консоль
+const url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23urfu-testing-2016';
+
+/**
+ * Выводит на консоль список твитов
+ * @param {Func} cb
+ */
+function showTweets(cb) {
+    request(url, (requestError, res, body) => {
+        if (requestError || res.statusCode !== 200) {
+            console.error(res.status);
+            cb();
+        }
+        try {
+            const tweets = JSON.parse(body);
+            if (tweets) {
+                tweets.forEach((tweet) => {
+                    console.log(formatDate(tweet.created_at));
+                    console.log(tweet.text);
+                });
+            }
+        } catch (parseError) {
+            console.error(parseError);
+        }
+        cb();
+    });
 }
 
 module.exports = showTweets;
