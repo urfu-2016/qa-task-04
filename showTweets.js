9d20e24d98eb873593d3d765f8f69fda301c88fe
Георгий Бородин < >
2017-05-01 12:00:45 +0500

    Реализовал showTweets

--- showTweets.js
+++ showTweets.js
@@ -1,8 +1,35 @@
+const request = require('request');
 const formatDate = require('./formatDate');
 
-function showTweets() {
-    // Здесь будет код, который получает твиты и
-    // выводит их на консоль
+const url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23urfu-testing-2016';
+
+// Здесь будет код, который получает твиты и
+// выводит их на консоль
+
+function showTweets(cb) {
+    request(url, (requestError, res, body) => {
+        if (requestError) {
+            console.error(requestError);
+            cb();
+            return;
+        } else if (res.statusCode !== 200) {
+            console.error(res.status);
+            cb();
+            return;
+        }
+        try {
+            const tweets = JSON.parse(body);
+            tweets.forEach((tweet) => {
+
+                let result = `${formatDate(tweet.created_at)}\n"${tweet.text}"`;
+                console.log(result);
+
+            });
+        } catch (parseError) {
+            console.error(parseError);
+        }
+        cb();
+    });
 }
 
 module.exports = showTweets;
