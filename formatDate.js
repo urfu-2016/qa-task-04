b4b68cfd3589aa39b994da644ad8b3353c933194
nikitc <nikitc6@rambler.ru>
2017-05-02 23:29:11 +0500

    Добавлено решение задачи 4

--- formatDate.js
+++ formatDate.js
@@ -1,5 +1,50 @@
+const MONTHS = ['января', 'февраля', 'марта', 'апреля', 'мая',
+                    'июня', 'июля', 'августа', 'сентября',
+                    'октября', 'ноября', 'декабря'];
+
 function formatDate(date) {
-    // Напишите код форматирования даты в этом месте
+    const chooseDate = new Date(date);
+    const todayDate = new Date();
+
+    if (chooseDate.toString() === 'Invalid Date' ||
+        todayDate.toString() === 'Invalid Date') {
+        throw new TypeError('Date should not invalid');
+    }
+    const chooseTime = `${chooseDate.getHours()}:${chooseDate.getMinutes()}`;
+    const chooseDay =  `${chooseDate.getDate()} ${MONTHS[chooseDate.getMonth()]}`;
+    const chooseFullDate = `${chooseDay} ${chooseDate.getFullYear()} года в ${chooseTime}`;
+
+    if (chooseDate > todayDate) {
+        throw new RangeError('Choose date should be earlier today');
+    }
+
+    if (isToday(chooseDate, todayDate)) {
+        return chooseTime;
+    }
+
+    if (isYesterday(chooseDate, todayDate)) {
+        return `вчера в ${chooseTime}`;
+    }
+
+    if (isThisYear(chooseDate, todayDate)) {
+        return `${chooseDay} в ${chooseTime}`;
+    }
+
+    return chooseFullDate;
 }
+
+function isToday(chooseDate, todayDate) {
+    return chooseDate.toDateString() === todayDate.toDateString();
+}
+
+function isYesterday(chooseDate, todayDate) {
+    todayDate.setDate(todayDate.getDate() - 1);
+    return isToday(chooseDate, todayDate);
+}
+
+function isThisYear(chooseDate, todayDate) {
+    return chooseDate.getFullYear() == todayDate.getFullYear();
+}
+
 
 module.exports = formatDate;
--- showTweets.js
+++ showTweets.js
@@ -1,8 +1,22 @@
 const formatDate = require('./formatDate');
+const rp = require('request-promise');
+const nock = require('nock');
+
+
+function printTweet(tweet) {
+    console.log(formatDate(tweet['created_at']));
+    console.log(tweet['text']);
+}
 
 function showTweets() {
-    // Здесь будет код, который получает твиты и
-    // выводит их на консоль
+    var options = {
+      uri: 'https://api.twitter.com/1.1/search/tweets.json',
+        qs: {q: '#urfu-testing-2016'}
+    };
+
+    return rp(options)
+        .then(JSON.parse)
+        .then(x => x.forEach(printTweet));
 }
 
 module.exports = showTweets;
