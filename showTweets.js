4676371b48c44143740517fc0908739e6c7858da
nikshel <nikshel@yandex-team.ru>
2017-05-03 10:55:01 +0500

    Консольный твиттер

--- showTweets.js
+++ showTweets.js
@@ -1,8 +1,18 @@
+'use strict';
+
+const got = require('got');
+
 const formatDate = require('./formatDate');
 
+const URL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23urfu-testing-2016';
+
 function showTweets() {
-    // Здесь будет код, который получает твиты и
-    // выводит их на консоль
+    return got(URL, { json: true })
+        .then(({ body: tweets }) => {
+            for (const { created_at, text } of tweets) {
+                console.log(`${formatDate(created_at)}\n"${text}"`);
+            }
+        });
 }
 
 module.exports = showTweets;
