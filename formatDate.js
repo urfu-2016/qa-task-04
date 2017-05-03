6da4102e271d525aef628fb7dc2a81e70ae37559
Mikhail Pliskovsky <snoward@yandex.ru>
2017-05-03 17:44:03 +0500

    Реализовал форматирование даты

--- formatDate.js
+++ formatDate.js
@@ -1,5 +1,42 @@
-function formatDate(date) {
-    // Напишите код форматирования даты в этом месте
+'use strict';
+
+const MONTHS = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+                 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+
+Date.prototype.addDays = function(days) {
+  var newDate = new Date(this.valueOf());
+  newDate.setDate(newDate.getDate() + days);
+  
+  return newDate;
 }
 
-module.exports = formatDate;
+/**
+ * @param {Date} date
+ * @return {String} formatted date
+ */
+function formatDate(date, todayDate = new Date()) {
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const month = MONTHS[date.getMonth()];
+    const hoursAndMinutes = date.toLocaleTimeString('ru', {hour:'2-digit', minute:'2-digit'}); 
+    
+    return haveSameDates(date, todayDate)            ? hoursAndMinutes 
+         : haveSameDates(date.addDays(1), todayDate) ? `вчера в ${hoursAndMinutes}`
+         : year === todayDate.getFullYear()          ? `${day} ${month} в ${hoursAndMinutes}`
+         : `${day} ${month} ${year} года в ${hoursAndMinutes}`;
+}
+
+/**
+ * Checks if two dates have same DD:MM:YYYY format
+ * @param {Date} date
+ * @param {Date} anotherDate
+ * @return {Bool} result of format comparison
+ */
+function haveSameDates(date, anotherDate) {
+    return date.toLocaleDateString() === anotherDate.toLocaleDateString();
+}
+
+module.exports = {
+    formatDate: formatDate,
+    haveSameDates: haveSameDates
+}
---
+++ tests/formatDate-test.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+const formatDate = require('../formatDate');
+
+describe('formatDate', () => {
+    it('haveSameDates should return true if dates have equal DD:MM:YYYY', () => {
+        var todayMorning = new Date('January 1, 2017 07:00:00');
+        var todayEvening = new Date('January 1, 2017 19:00:00');
+
+        var actualResult = formatDate.haveSameDates(todayMorning, todayEvening);
+    
+        assert.equal(actualResult, true);
+    });
+
+    it('haveSameDates should return false if dates has not equal DD:MM:YYYY', () => {
+        const todayMorning = new Date('January 1, 2017');
+        const tommorowEvening = new Date('January 2, 2017');
+
+        const actualResult = formatDate.haveSameDates(todayMorning, tommorowEvening);
+    
+        assert.equal(actualResult, false);
+    });
+
+    it('should print only time if day is today', () => {
+        const todayMorning = new Date('January 1, 2017 07:00:00');
+        const todayEvening = new Date('January 1, 2017 19:00:00');
+
+        const actualResult = formatDate.formatDate(todayMorning, todayEvening);
+
+        assert.equal(actualResult, '07:00');
+    });
+
+    it('should print `вчера в ЧЧ:MM` when day is yesterday', () => {
+        const yesterdayMorning = new Date('January 1, 2017 07:00:00');
+        const todayEvening = new Date('January 2, 2017 19:00:00');
+
+        const actualResult = formatDate.formatDate(yesterdayMorning, todayEvening);
+
+        assert.equal(actualResult, 'вчера в 07:00');
+    });
+
+    it('should pring `ДД МЕСЯЦ в ЧЧ:ММ` when date is > 1 days earlier', () => {
+        const today = new Date('January 10, 2017 12:00:00');
+        const dayBeforeYesterday = new Date('January 8, 2017 12:00:00');
+
+        const actualResult = formatDate.formatDate(dayBeforeYesterday, today);
+
+        assert.equal(actualResult, '8 января в 12:00');
+    }); 
+
+    it('should pring `ДД МЕСЯЦ ГГГГ года в ЧЧ:ММ` when date is more than 1 year earlier', () => {
+        const today = new Date('January 1, 2017 12:00:00');
+        const dayBeforeYesterday = new Date('January 1, 2016 12:00:00');
+
+        const actualResult = formatDate.formatDate(dayBeforeYesterday, today);
+
+        assert.equal(actualResult, '1 января 2016 года в 12:00');
+    });
+});
