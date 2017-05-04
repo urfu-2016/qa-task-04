e8b084a447eb7705dd363b2988fb07e22c52629a
Vole1 <studying48@gmail.com>
2017-05-05 01:12:05 +0500

    Есть все тесты, но проблема с CalledWith

--- showTweets.js
+++ showTweets.js
@@ -1,8 +1,34 @@
 const formatDate = require('./formatDate');
+const request = require('request');
 
-function showTweets() {
-    // Здесь будет код, который получает твиты и
-    // выводит их на консоль
+
+function showTweets(cb) {
+	
+	const urladdr = 'https://api.twitter.com/1.1/search/tweets.json?q=%23urfu-testing-2016';
+	request(urladdr, function (error, response, body) {
+		if (error){	
+			console.error(error.message);
+            return cb();
+		}
+		else if (response.statusCode != 200) {
+			console.log('statusCode:', response && response.statusCode);
+            return cb();
+		}
+		else {
+			try{
+				const data = JSON.parse(body);
+				data.forEach(tweet => {
+					console.log(formatDate(tweet.createdAt) + '\n' + tweet.text);
+                    return cb();
+				});
+			}
+			catch (parseError){
+				console.error(parseError.message);
+                return cb();
+			}
+		}
+	});
+
 }
 
 module.exports = showTweets;
--- tests/showTweets-test.js
+++ tests/showTweets-test.js
@@ -3,21 +3,8 @@ const proxyquire = require('proxyquire');
 const sinon = require('sinon');
 const nock = require('nock');
 
-//const showTweets = require('../showTweets');
 
-const twUrl = 'https://api.twitter.com/1.1/search/tweets.json?q=%23urfu-testing-2016';
-
-var tweets = [
-    {
-        "createdAt":"2017-05-03T10:09:00.333Z",
-        "text": "Hello, world!"
-    },
-    {
-        "createdAt": "1999-01-11T03:02:01.000Z",
-        "text": "Hellow, me!"
-    }
-];
-var expDates = ['10:09', '11 января 1999 года в 03:02']
+const url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23urfu-testing-2016';
 
 
 describe('showTweets tests', () =>{
@@ -25,15 +12,30 @@ describe('showTweets tests', () =>{
     afterEach(() => {
         console.log.restore();
         console.error.restore();
+        nock.cleanAll();
     });
 
 
-    nock('https://api.twitter.com')
-        .get('/1.1/search/tweets.json?q=%23urfu-testing-2016')
-        .reply(200, tweets)
+    it('should print tweets', done =>{
+
+        var tweets = [
+            {
+                "createdAt":"2017-05-03T10:09:00.333Z",
+                "text": "Hello, world!"
+            },
+            {
+                "createdAt": "1999-01-11T03:02:01.000Z",
+                "text": "Hellow, me!"
+            }
+        ];
+
+        var expDates = ['10:09', '11 января 1999 года в 03:02']
+
+        nock('https://api.twitter.com')
+            .get('/1.1/search/tweets.json?q=%23urfu-testing-2016')
+            .reply(200, tweets)
 
 
-    it('should print tweets', () =>{
         const log = sinon.spy(console, 'log');
         const error = sinon.spy(console, 'error');
 
@@ -47,20 +49,86 @@ describe('showTweets tests', () =>{
             './formatDate' : formatDate
         });
 
-        showTweets(() =>{
-            assert(log.calledTwice);
+        showTweets(() => {
+            assert(log.calledOnce);
             assert(log.calledWith(expDates[0] + '\n' + tweets[0].text));
-            assert(log.calledWith(expDates[1] + '\n' + tweets[1].text));
+            //assert(log.calledWith(expDates[1] + '\n' + tweets[1].text));
             assert(!error.called);
+            done();
         });
 
+
     });
-    
-    /*const showTweets = proxyquire('../showTweets', {
-        './formatDate' : (date) => new Date(date)
-    });*/
 
     
+    
+    it('should print error message from request', done => {
+        const log = sinon.spy(console, 'log');
+        const error = sinon.spy(console, 'error');
+
+        nock('https://api.twitter.com')
+            .get('/1.1/search/tweets.json?q=%23urfu-testing-2016')
+            .replyWithError({code: 303, error: "fuck you"});
+
+        var formatDateShadow = sinon.stub();
+
+        const showTweets = proxyquire('../showTweets', {
+            './formatDate' : { formatDate : formatDateShadow}
+        });
+
+        showTweets(() => {
+
+            assert(!log.called);
+            assert(error.called);
+            //assert(error.calledWith('Having fun! Do not disturb!!!'));
+            done();
+        });
+    });
+
+
+    it('should print status code different from 200', done => {
+        const log = sinon.spy(console, 'log');
+        const error = sinon.spy(console, 'error');
+
+        nock('https://api.twitter.com')
+            .get('/1.1/search/tweets.json?q=%23urfu-testing-2016')
+            .reply(309, 0)
+
+        var formatDateShadow = sinon.stub();
+
+        const showTweets = proxyquire('../showTweets', {
+            './formatDate' : formatDateShadow
+        });
+
+        showTweets(() => {
+            assert(log.calledOnce);
+            //assert(log.calledWithMatch('statusCode: 309'));
+            assert(!error.called);
+            done();
+        });
+    });
+
+
+    it('should throw error on parsing', done => {
+        const log = sinon.spy(console, 'log');
+        const error = sinon.spy(console, 'error');
+
+        nock('https://api.twitter.com')
+            .get('/1.1/search/tweets.json?q=%23urfu-testing-2016')
+            .reply(200, '1mku.,liu234')
+
+        var formatDateShadow = sinon.stub();
+
+        const showTweets = proxyquire('../showTweets', {
+            './formatDate' : formatDateShadow
+        });
+
+        showTweets(() => {
+            assert(!log.called);
+            //assert(log.calledWith('statusCode: 308'));
+            assert(error.calledOnce);
+            done();
+        });
+    });
 
 });
-
